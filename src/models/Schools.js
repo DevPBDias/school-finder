eda962b364acbdd9ecba05223723de18972d7bae
DevPBDias <devpbdias@gmail.com>
2024-02-23 00:04:41 -0300

    feat: create school model

---
+++ src/models/Schools.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+
+const SchoolSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    neighborhood: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    adress: {
+        type: String,
+        required: true,
+    },
+    schoolType: {
+        type: String,
+        required: true,
+    },
+    about: {
+        type: String,
+        required: true,
+    },
+    schoolFeedback: {
+        type: [Number],
+        required: true,
+    },
+    educationType: {
+        kindergarten: {
+            vacancies: { type: Number },
+            shifts: { type: String },
+            schoolFee: { type: Number }
+        },
+        primarySchool: {
+            vacancies: { type: Number },
+            shifts: { type: String },
+            schoolFee: { type: Number }
+        },
+        secondarySchool: {
+            vacancies: { type: Number },
+            shifts: { type: String },
+            schoolFee: { type: Number }
+        },
+        highSchool: {
+            vacancies: { type: Number },
+            shifts: { type: String },
+            schoolFee: { type: Number }
+        },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+const School = mongoose.model("School", SchoolSchema);
+
+export default School;
