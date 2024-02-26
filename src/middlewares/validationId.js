2b9d7a68af903a384f54726ed0b851dd3f309089
DevPBDias <devpbdias@gmail.com>
2024-02-26 11:02:56 -0300

    feat: create middleware to validate id

---
+++ src/middlewares/validationId.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const validId = (req, res, next) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid id!" });
+  }
+  next();
+};
