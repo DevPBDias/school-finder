2736722f7a97fdb83fe159f3a641dde751edd7e1
DevPBDias <devpbdias@gmail.com>
2024-02-26 11:49:31 -0300

    feat: created school routes

---
+++ src/routes/school.route.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import schoolController from '../controllers/school.controller.js'
+import validationSchool from '../middlewares/validationId.js'
+
+const schoolRoute = Router();
+
+schoolRoute.get('/', schoolController.getAllSchools)
+schoolRoute.get('/:id', validationSchool, schoolController.getSchoolById)
+schoolRoute.post('/', schoolController.createSchool)
+schoolRoute.patch('/:id', validationSchool, schoolController.addComment)
+
+export default schoolRoute;
